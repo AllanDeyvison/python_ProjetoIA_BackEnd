e74dcf2526a65adab714a11d4c22c6ca53d4cc0b
Beatriz Medeiros Faria <medeirosfbia@gmail.com>
2025-09-18 22:03:55 -0300

    adding different contexts

--- services/chat_service.py
+++ services/chat_service.py
@@ -10,8 +10,8 @@ from models.history import (
 )
 
 LLM_MODEL = os.getenv('LLM_MODEL')
-math_context = ""
-english_context = ""
+math_context = "Você é um assistente especializado em resolver problemas matemáticos. Responda com o passo a passo de forma clara e concisa. Responda sempre em Português Brasileiro e organize em MarkDown. Nunca dê a resposta ao final, apenas o passo a passo. Se não souber a resposta, diga que não sabe. Caso a pergunta não tenha relação com matemática, diga que não sabe e não tente responder."
+english_context = "You are an assistant specialized in helping with english study. Answer with a clear and concise step-by-step explanation. Prefer to respond in english but translate if the student is facing dificulties. Never provide the final answer, only the step-by-step process. If you don't know the answer, say that you don't know. If the question is related to mathematics, say that you don't know and do not attempt to answer."
 
 def query_old(query):
     response = ollama.chat(model='qwen2', messages=[
@@ -34,10 +34,17 @@ def query_new_chat(user_id: str, model:str, query: str) -> str:
     try:
         if model:
             LLM_MODEL = model
+            
+        if model == "math":
+            context = math_context
+        elif model == "english":
+            context = english_context
+        
+        
         response = ollama.chat(model=LLM_MODEL, messages=[
             {
                 "role": "system",
-                'content': 'Você é o AprovIA um assistente especializado em resolver problemas matemáticos. Responda com o passo a passo de forma clara e concisa. Responda sempre em Português Brasileiro e organize em MarkDown. Nunca dê a resposta ao final, apenas o passo a passo. Se não souber a resposta, diga que não sabe. Caso a pergunta não tenha relação com matemática, diga que não sabe e não tente responder.',
+                'content': context,
             },
             {"role": "user", "content": query}
         ])
@@ -54,9 +61,15 @@ def query_new_chat(user_id: str, model:str, query: str) -> str:
 
 def continue_chat(user_id: str, chat_id: str, model:str, query: str) -> str:
     chat = get_chat_messages(user_id, chat_id)
+    
+    if model == "math":
+            context = math_context
+    elif model == "english":
+            context = english_context
+            
     messages = [{
         "role": "system", 
-        "content": "Você é um assistente especializado em resolver problemas matemáticos. Responda com o passo a passo de forma clara e concisa. Responda sempre em Português Brasileiro e organize em MarkDown. Nunca dê a resposta ao final, apenas o passo a passo. Se não souber a resposta, diga que não sabe. Caso a pergunta não tenha relação com matemática, diga que não sabe e não tente responder. A partir disso continue a explicação matemática:"}]
+        "content": context}]
     messages.extend(chat["messages"][-10:])
     messages.append({"role": "user", "content": query})
     try:
